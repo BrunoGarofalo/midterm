4e8b1a0a5d6e09166dcd76481b85fb0a9af38ff9
BrunoGarofalo <garofalobruno9@gmail.com>
2025-10-16 09:07:19 -0400

    added function to validate operands are numeric

--- app/repl.py
+++ app/repl.py
@@ -1,6 +1,17 @@
 from operation_selection import operationSelection
 from app.command_factory import CommandFactory
+from decimal import Decimal, InvalidOperation
 
+def get_decimal_input(prompt):
+    while True:
+        #get user input
+        value = input(prompt)
+        try:
+            #try to convert to decimal, if it fails then the value passed is not a number
+            return Decimal(value)
+        #return an error if this happens
+        except InvalidOperation:
+            print("Invalid input. Please enter a numeric value.")
 
 def main():
     print('Welcome to the Calculator app!')
@@ -22,10 +33,10 @@ def main():
         if hasattr(operation_obj, "runOperation"):
             try:
                 # ask user to input operand A
-                operand_a = float(input("Enter first operand: "))
+                operand_a = get_decimal_input("Enter first operand: ")
 
                 # ask user to input operand B
-                operand_b = float(input("Enter second operand: "))
+                operand_b = get_decimal_input("Enter second operand: ")
 
                 #Pass operands to check)decimals method to validate that the entries are valid, if not an error will be raised
                 operation_obj.check_decimals(operand_a, operand_b)
@@ -34,15 +45,10 @@ def main():
                 results = operation_obj.runOperation(operand_a, operand_b)
 
                 #display the result
-                print(f"Result: {results}")
+                print(f"Result of {operation_code} with operands {operand_a} and {operand_b} = {results}")
 
             except  Exception as e:
                 print(f"Error: {e}")
 
 
-        # factory = CommandFactory(selectedOperationcode)
 
-        # operation_object = factory.createOperationObject()
-
-
-main()
