b5e429e5d866d0b7e41224b5a91568c454c789d1
BrunoGarofalo <garofalobruno9@gmail.com>
2025-10-17 09:23:21 -0400

    updated REPL to include Observer actions, still missing Memento actions

--- app/repl.py
+++ app/repl.py
@@ -1,6 +1,8 @@
-from operation_selection import operationSelection
+from app.operation_selection import operationSelection
 from app.command_factory import CommandFactory
 from decimal import Decimal, InvalidOperation
+from app.observers import LoggingObserver, Subject, AutosaveObserver
+from datetime import datetime
 
 def get_decimal_input(prompt):
     while True:
@@ -14,6 +16,16 @@ def get_decimal_input(prompt):
             print("Invalid input. Please enter a numeric value.")
 
 def main():
+
+    logging_observer = LoggingObserver()
+    autosave_observer = AutosaveObserver()
+
+    subject_logging = Subject()
+    subject_autosave = Subject()
+
+    subject_logging.attach(logging_observer)
+    subject_autosave.attach(autosave_observer)
+
     print('Welcome to the Calculator app!')
 
     while True:
@@ -21,8 +33,32 @@ def main():
         operation_code = selection.determineOperationCode()
 
         if operation_code == "exit":
+            logging_observer.delete_history()
             print("Goodbye!!")
             break
+
+        if operation_code=='save':
+            logging_observer.save_history()
+            continue
+
+        # show history of logging observer
+        if operation_code == 'hist': 
+            logging_observer.show_history()
+            continue
+            
+        #clear history of logging observer
+        if operation_code == 'clear':
+            # autosave_observer.delete_history()
+            logging_observer.delete_history()
+            continue
+
+        # load history from autosave observer to logging observer
+        if operation_code == 'load':
+            history = autosave_observer.load_history()
+            logging_observer.history = history
+            print('History loaded!')
+            continue
+
 
         try:
             operation_obj = CommandFactory(operation_code).createOperationObject()
@@ -44,11 +80,21 @@ def main():
                 #Get results
                 results = operation_obj.runOperation(operand_a, operand_b)
 
+                #logger message
+                timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                final_message = f"{timestamp}|{operation_obj.__class__.__name__}|{operand_a}|{operand_b}|{results} "
+
+                #notify observers
+                subject_autosave.notify(final_message)
+                subject_logging.notify(final_message)
+
+                print(final_message)
+
                 #display the result
-                print(f"Result of {operation_code} with operands {operand_a} and {operand_b} = {results}")
+                print(f"✅ Result of {operation_code} with operands {operand_a} and {operand_b} = {results}")
 
             except  Exception as e:
-                print(f"Error: {e}")
+                print(f"❌ Error: {e}")
 
 
 
