36fe59cd0f446beb3cb98b126ab849b36e88f9f0
BrunoGarofalo <garofalobruno9@gmail.com>
2025-10-18 10:37:50 -0400

    Uploaded first draft of memento, I may remove notes later. I find them useful to remember what is what

---
+++ app/memento.py
@@ -0,0 +1,152 @@
+from copy import deepcopy
+
+class MementoCalculator:
+    #hold snapshop in time
+    def __init__(self, state):
+        # state set as private variable, deecopy will prevent that the history is modified outside
+        '''self._state holds a snapshot of the entire history up to the moment the memento is created
+        _state: [5+2=7, 3*4=12, 10-1=9]   <-- snapshot of history at this point'''
+        self._state = deepcopy(state)
+
+    def get_state(self):
+        #gets the actual history of mementos or history snapshots
+        '''
+        gets the actual memento
+        Mementos (snapshots):
+        M1: ["5 + 2 = 7"]
+        M2: ["5 + 2 = 7", "3 * 4 = 12"]
+        M3: ["5 + 2 = 7", "3 * 4 = 12", "10 - 1 = 9"]
+
+        IE: M2.get_state() returns ["5 + 2 = 7", "3 * 4 = 12"]
+        '''
+        return deepcopy(self._state)
+    
+class Originator: 
+    #the object whose state we want to track, holds the current history and creates or restores mementos
+    def __init__(self):
+        self.history = []
+
+    def create_memento(self):
+        # create a new memento that reflect the current history state up to that point in time
+        '''
+        History grows ──▶
+        ["5 + 2 = 7"] ──▶ ["5 + 2 = 7", "3 * 4 = 12"] ──▶ ["5 + 2 = 7", "3 * 4 = 12", "10 - 1 = 9"]
+
+        Mementos (snapshots):
+        M1: ["5 + 2 = 7"]
+        M2: ["5 + 2 = 7", "3 * 4 = 12"]
+        M3: ["5 + 2 = 7", "3 * 4 = 12", "10 - 1 = 9"]
+        '''
+        return MementoCalculator(self.history)
+    
+    def add_operation(self, message):
+        #saves new operation to temporary history, IE: "12 - 2 = 10"
+        self.history.append(message)
+    
+    def restore_memento(self, memento):
+        #restore history from a previous memento
+        '''
+        updates the current history with the selected memento snapshot
+        IE: M2.get_state() returns self.history = ["5 + 2 = 7", "3 * 4 = 12"]
+        '''
+        self.history = memento.get_state()
+
+class CareTaker:
+    # manage stack redo and undo
+    '''
+    Caretaker does NOT know the details of the state.
+    Its job is to store Mementos and let the Originator restore them later.
+    It's like a history manager: it keeps track of all the snapshots taken.
+    '''
+    def __init__(self):
+        '''
+        An undo stack is basically a history of previous states stored in a last-in, first-out (LIFO) structure,
+        usually implemented as a list or stack. Its purpose is to let you go backward in time—undo actions you’ve performed.
+        
+        How it works: when you perform operations normally. The undo stack grows; the redo stack is empty.
+        '''
+        self.stack_undo = []
+
+        '''
+        The redo stack keeps track of states you can return to after an undo. It's as “forward history”: after you undo something,
+        redo lets you reapply that change. Without a redo stack, once you undo, the undone state is lost.
+        '''
+        self.stack_redo = []
+
+
+
+    def save_memento(self, memento):
+        self.stack_undo.append(memento)
+        
+        #clear stack redo when a new action is done
+        self.stack_redo.clear()
+
+    def undo_memento(self, originator):
+
+        '''
+        Undo: pop from stack_undo → push current state to stack_redo → restore popped memento
+
+        Starting point:
+        - history = ["5 + 2 = 7", "3 * 4 = 12"]
+        - undo_stack = [ [], ["5 + 2 = 7"] ]
+        - redo_stack = []
+
+        When you undo, you:
+        - Pop last state from undo stack → ["5 + 2 = 7"]
+        - Push current history onto redo stack → ["5 + 2 = 7", "3 * 4 = 12"]
+        - Restore popped state → ["5 + 2 = 7"]
+        
+        - history = ["5 + 2 = 7"]
+        - undo_stack = [ [] ]
+        - redo_stack = [ ["5 + 2 = 7", "3 * 4 = 12"] ]
+
+        Notice the most recent state is always in the history (top). The previous state is in the undo stack
+        '''
+        if not self.stack_undo:
+            print("No operation to undo!")
+            return
+
+        #take the last memento from the stack_undo
+        memento = self.stack_undo.pop()
+
+        # add memento for new state to stack_redo
+        self.stack_redo.append(originator.create_memento())
+
+        # restore popped memento from stack_undo to history
+        originator.restore_memento(memento)
+
+    def redo_memento(self, originator):
+        '''
+        Redo: pop from stack_redo → push current state to stack_undo → restore popped memento
+
+        Starting point:
+        - history = ["5 + 2 = 7"]
+        - undo_stack = [ [] ]
+        - redo_stack = [ ["5 + 2 = 7", "3 * 4 = 12"] ]
+
+        When you redo:
+
+        - Pop last state from redo stack → ["5 + 2 = 7", "3 * 4 = 12"]
+        - Push current history onto undo stack → ["5 + 2 = 7"]
+        - Restore popped state to history → ["5 + 2 = 7", "3 * 4 = 12"]
+
+        - history = ["5 + 2 = 7", "3 * 4 = 12"]
+        - undo_stack = [ [], ["5 + 2 = 7"] ]
+        - redo_stack = []
+        '''
+        if not self.stack_redo:
+            print("No operation to redo!")
+            return
+        
+        #take the last memento from the stack_redo
+        memento = self.stack_redo.pop()
+
+        # add memento for new state to stack_redo
+        self.stack_undo.append(originator.create_memento())
+
+        # restore popped memento from stack_redo to history
+        originator.restore_memento(memento)
+
+
+
+    
