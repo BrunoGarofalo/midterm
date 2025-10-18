06cc44e94d18dd70df10b92a8878281a9002f11b
BrunoGarofalo <garofalobruno9@gmail.com>
2025-10-18 18:42:28 -0400

    simplified class and moved critical code to repl.py

--- app/operation_selection.py
+++ app/operation_selection.py
@@ -23,24 +23,41 @@ class operationSelection:
                             'Q': ['Display available commands', 'help'],
                             'R': ['Exit the program', 'exit']}
 
-    user_input_message = "\n".join([f"{key:<2}: {value[0]}" for key, value in operations_dictionary.items()])
+    # user_input_message = "\n".join([f"{key:<2}: {value[0]}" for key, value in operations_dictionary.items()])
 
-    def __init__(self):
-        while True:
-            self.user_input = input(f'👉 Please select one of the following commands:\n {self.user_input_message}\n').upper()
+    # def __init__(self):
+    #     print("👋 Welcome! Type 'help' to see the list of available commands.")
+    #     while True:
+    #         self.user_input = input(f'👉 Please select one of the following commands:\n {self.user_input_message}\n').upper()
 
-            if self.user_input in self.operations_dictionary:
-                logger.info(f"User selected command: {self.user_input} - {self.operations_dictionary[self.user_input][0]}")
-                break
-            else:
-                logger.warning(f"Invalid command entered by user: {self.user_input}")
-                print(f"❌ Command {self.user_input} not available!")
+    #         if self.user_input in self.operations_dictionary:
+    #             logger.info(f"User selected command: {self.user_input} - {self.operations_dictionary[self.user_input][0]}")
+    #             break
+    #         else:
+    #             logger.warning(f"Invalid command entered by user: {self.user_input}")
+    #             print(f"❌ Command {self.user_input} not available!")
                 
 
-    def determineOperationCode(self):
-        self.operation_code = self.operations_dictionary[self.user_input][1].lower()
-        logger.info(f"Determined operation code: {self.operation_code} for command {self.user_input}")
-        print(f"selected command: {self.user_input} - {self.operation_code}")
-        return self.operation_code
+    # def determineOperationCode(self):
+    #     self.operation_code = self.operations_dictionary[self.user_input][1].lower()
+    #     logger.info(f"Determined operation code: {self.operation_code} for command {self.user_input}")
+    #     print(f"selected command: {self.user_input} - {self.operation_code}")
+    #     return self.operation_code
+
+    @classmethod
+    def show_commands(cls):
+        """Return a formatted string of all available commands."""
+        return "\n".join([f"{key:<2}: {value[0]}" for key, value in cls.operations_dictionary.items()])
+
+    @classmethod
+    def get_operation_code(cls, user_input: str):
+        """Return the operation code (short name) for a given input letter."""
+        if user_input in cls.operations_dictionary:
+            op_code = cls.operations_dictionary[user_input][1]
+            logger.info(f"Determined operation code '{op_code}' for command '{user_input}'")
+            return op_code
+        else:
+            logger.warning(f"Invalid operation key: {user_input}")
+            return None
 
 
